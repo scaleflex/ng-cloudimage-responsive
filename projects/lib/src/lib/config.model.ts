42d70dfae1bb49b17550bd2862d151d980f851fe
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2019-02-20 17:12:02 +0200

    Refactoring

---
+++ projects/lib/src/lib/config.model.ts
@@ -0,0 +1,16 @@
+export class CIConfig {
+  token: string;
+  container: string;
+  ultraFast: boolean;
+  lazyLoading: boolean;
+  imgLoadingAnimation: boolean;
+  lazyLoadOffset: number;
+  width: string;
+  height: string;
+  operation: string;
+  filters: string;
+  placeholderBackground: string;
+  baseUrl: string;
+  presets: any;
+  queryString: string;
+}
--- projects/lib/src/public_api.ts
+++ projects/lib/src/public_api.ts
@@ -3,6 +3,6 @@
  */
 
 export * from './lib/lib.service';
-export * from './lib/config.service';
+export * from './lib/config.model';
 export * from './lib/lib.module';
 export * from './lib/img/img.component';
