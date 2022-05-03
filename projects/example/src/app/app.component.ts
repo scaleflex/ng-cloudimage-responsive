6cf34d191d5cf64f7c68f0188ebae00356939891
Tuan Duong <quangtuan9237@gmail.com>
2022-05-04 01:19:19 +0700

    refactor: using new logics from cloudimage-responsive-utils

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../assets/fonts/helvetica-neue.css']
+  styleUrls: ['./app.component.css', '../assets/fonts/helvetica-neue.css'],
 })
 export class AppComponent {
   initialization = `import { NgModule } from '@angular/core';
@@ -32,99 +32,99 @@ export class MyAppModule {}`;
     {
       src: 'luca-bravo-121932.jpg',
       ratio: 4.538 / 1.932,
-      original_size: '4.8mb'
+      original_size: '4.8mb',
     },
     {
       src: 'alain.jpg',
       ratio: 6 / 4,
-      original_size: '5.8mb'
+      original_size: '5.8mb',
     },
     {
       src: 'ameen-fahmy.jpg',
       ratio: 2.926 / 1.953,
-      original_size: '0.5mb'
+      original_size: '0.5mb',
     },
     {
       src: 'tim-patch.jpg',
-      ratio: 5.464 / 3.640,
-      original_size: '7.5mb'
+      ratio: 5.464 / 3.64,
+      original_size: '7.5mb',
     },
     {
       src: 'veeterzy.jpg',
-      ratio: 5.760 / 3.840,
-      original_size: '8.5mb'
+      ratio: 5.76 / 3.84,
+      original_size: '8.5mb',
     },
     {
       src: 'dino-reichmuth.jpg',
       ratio: 7.833 / 5.304,
-      original_size: '11.9mb'
+      original_size: '11.9mb',
     },
     {
       src: 'inma-lesielle.jpg',
       ratio: 4.032 / 2.688,
-      original_size: '3.0mb'
+      original_size: '3.0mb',
     },
     {
       src: 'jeremy-thomas.jpg',
       ratio: 5.005 / 3.417,
-      original_size: '5.2mb'
+      original_size: '5.2mb',
     },
     {
       src: 'jordan-hubbard.jpg',
       ratio: 5 / 3.333,
-      original_size: '3.0mb'
+      original_size: '3.0mb',
     },
     {
       src: 'jp-valery.jpg',
       ratio: 5.472 / 3.648,
-      original_size: '2.2mb'
+      original_size: '2.2mb',
     },
     {
       src: 'kira-laktionov.jpg',
       ratio: 4.032 / 2.688,
-      original_size: '2.6mb'
+      original_size: '2.6mb',
     },
     {
       src: 'michael-d-beckwith-6.jpg',
       ratio: 5.184 / 3.456,
-      original_size: '5.2mb'
+      original_size: '5.2mb',
     },
     {
       src: 'michael-d-beckwith.jpg',
       ratio: 8.192 / 5.461,
-      original_size: '9.2mb'
+      original_size: '9.2mb',
     },
     {
       src: 'ricky-kharawala.jpg',
       ratio: 8.192 / 5.461,
-      original_size: '9.2mb'
+      original_size: '9.2mb',
     },
     {
       src: 'rodolfo-marques.jpg',
       ratio: 6 / 4,
-      original_size: '8.2mb'
+      original_size: '8.2mb',
     },
     {
       src: 'dino-reichmuth-9.jpg',
       ratio: 6.616 / 3.744,
       b: 1.76,
-      original_size: '9.7mb'
+      original_size: '9.7mb',
     },
     {
       src: 'ishan-seefromthesky.jpg',
-      ratio: 5.464 / 3.070,
-      original_size: '4.2mb'
+      ratio: 5.464 / 3.07,
+      original_size: '4.2mb',
     },
     {
       src: 'dino-reichmuth-1.jpg',
       ratio: 7.952 / 5.622,
-      original_size: '9.2mb'
+      original_size: '9.2mb',
     },
     {
       src: 'dino-reichmuth-11.jpg',
-      ratio: 5.256 / 7.880,
-      original_size: '11mb'
-    }
+      ratio: 5.256 / 7.88,
+      original_size: '11mb',
+    },
   ];
 
   getDevicePixelRatio() {
--- projects/lib/src/lib/lib.service.ts
+++ projects/lib/src/lib/lib.service.ts
@@ -1,246 +1,98 @@
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformServer } from '@angular/common';
-import { CIConfig } from './config.model';
+import { CONSTANTS, processParams } from 'cloudimage-responsive-utils';
+import { CIConfig, CI_CONFIG } from './config.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CIService {
-  config: any = {};
   isSsr = false;
+  config: {
+    token: string;
+    domain: string | true;
+    customDomain: boolean;
+    lazyLoading: boolean;
+    lazyLoadOffset: number;
+    placeholderBackground: string;
+    baseURL: string;
+    apiVersion: string;
+    presets: { xs: string; sm: string; md: string; lg: string; xl: string };
+    ratio: number;
+    params: any;
+    imageSizeAttributes: string;
+    exactSize: boolean;
+    doNotReplaceURL: boolean;
+    limitFactor: number;
+    devicePixelRatioList: number[];
+    autoAlt: boolean;
+    previewQualityFactor: number;
+    minLowQualityWidth: number;
+    delay: number;
+  } = {} as any;
 
-  constructor(ciConfig: CIConfig, @Inject(PLATFORM_ID) private platformId: Object) {
-    this.isSsr = isPlatformServer(platformId);
+  constructor(
+    @Inject(CI_CONFIG) ciConfig: CIConfig,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isSsr = isPlatformServer(this.platformId);
 
     const {
       token = '',
-      container = 'cloudimg.io',
-      ultraFast = false,
+      domain = ciConfig.customDomain ? ciConfig.customDomain : 'cloudimg.io',
+      customDomain = false,
       lazyLoading = true,
-      imgLoadingAnimation = true,
       lazyLoadOffset = 100,
-      width = '400',
-      height = '300',
-      operation = 'width',
-      filters = 'foil1',
       placeholderBackground = '#f4f4f4',
-      baseUrl = '/',
+      baseUrl,
+      baseURL,
+      apiVersion = 'v7',
       presets,
-      queryString = ''
+      ratio = 1.5,
+      params = 'org_if_sml=1',
+      imageSizeAttributes = 'use',
+      exactSize = false,
+      doNotReplaceURL = false,
+      limitFactor = 100,
+      devicePixelRatioList = CONSTANTS.DEVICE_PIXEL_RATIO_LIST,
+      lowQualityPreview: { minImgWidth = 250 } = {},
+      autoAlt = false,
+      delay,
     } = ciConfig;
-    const windowInnerWidth = this.getWindowInnerWidth();
 
     this.config = {
       token,
-      container,
-      ultraFast,
+      domain,
+      customDomain,
       lazyLoading,
-      imgLoadingAnimation,
       lazyLoadOffset,
-      width,
-      height,
-      operation,
-      filters,
       placeholderBackground,
-      baseUrl,
-      presets: presets ? presets :
-        {
-          xs: '(max-width: 575px)',  // to 575       PHONE
-          sm: '(min-width: 576px)',  // 576 - 767    PHABLET
-          md: '(min-width: 768px)',  // 768 - 991    TABLET
-          lg: '(min-width: 992px)',  // 992 - 1199   SMALL_LAPTOP_SCREEN
-          xl: '(min-width: 1200px)'  // from 1200    USUALSCREEN
-        },
-      queryString,
-      innerWidth: windowInnerWidth,
-      previewQualityFactor: 10
-      // isChrome: /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)
+      baseURL: baseUrl || baseURL,
+      ratio,
+      exactSize,
+      presets: presets
+        ? presets
+        : {
+            xs: '(max-width: 575px)', // to 575       PHONE
+            sm: '(min-width: 576px)', // 576 - 767    PHABLET
+            md: '(min-width: 768px)', // 768 - 991    TABLET
+            lg: '(min-width: 992px)', // 992 - 1199   SMALL_LAPTOP_SCREEN
+            xl: '(min-width: 1200px)', // from 1200    USUALSCREEN
+          },
+      params: processParams(params),
+      apiVersion,
+      previewQualityFactor: 10,
+      doNotReplaceURL,
+      devicePixelRatioList,
+      limitFactor,
+      minLowQualityWidth: minImgWidth,
+      imageSizeAttributes,
+      autoAlt,
+      delay,
     };
-  }
-
-  getParentWidth(img, config): number {
-    if (!(img && img.parentElement && img.parentElement.getBoundingClientRect) && !(img && img.width)) {
-      return config.width;
-    }
-
-    const parentContainer = this.getParentContainerWithWidth(img);
-    const currentWidth = parseInt('' + parentContainer, 10);
-    const computedWidth = Number(this.getComputedStyle(img).width);
-
-    if ((computedWidth && (computedWidth < currentWidth && computedWidth > 15) || !currentWidth)) {
-      return this.getSizeLimit(computedWidth);
-    } else {
-      if (!currentWidth) {
-        return img.width || config.width;
-      }
-
-      return this.getSizeLimit(currentWidth);
-    }
-  }
-
-  getParentContainerWithWidth(img): number {
-    let parentNode = null;
-    let width = 0;
-
-    do {
-      parentNode = (parentNode && parentNode.parentNode) || img.parentNode;
-      width = parentNode.getBoundingClientRect().width;
-    } while (parentNode && !width);
-
-    const letPadding = width && parentNode && parseInt(this.getComputedStyle(parentNode).paddingLeft, 10);
-    const rightPadding = parseInt(this.getComputedStyle(parentNode).paddingRight, 10);
-
-    return width + (width ? (-letPadding - rightPadding) : 0);
-  }
-
-  getSizeLimit(currentSize): number {
-    if (currentSize <= 25) {
-      return 25;
-    }
-    if (currentSize <= 50) {
-      return 50;
-    }
-
-    return (Math.ceil(currentSize / 100) * 100);
-  }
-
-  checkOnMedia(size): boolean {
-    try {
-      const array = size.split(',');
-
-      return array.length > 1;
-    } catch (e) {
-      return false;
-    }
-  }
-
-  checkIfRelativeUrlPath(src): boolean {
-    if (src.indexOf('//') === 0) {
-      const protocol = this.isSsr ? 'https:' : window.location.protocol;
-      src = protocol + src;
-    }
-
-    return (src.indexOf('http://') !== 0 && src.indexOf('https://') !== 0 && src.indexOf('//') !== 0);
-  }
-
-  getImgSrc(src, isRelativeUrlPath = false, baseUrl = ''): string {
-    if (isRelativeUrlPath) {
-      return baseUrl + src;
-    }
-
-    return src;
-  }
-
-  getSizeAccordingToPixelRatio(size): string {
-    const splittedSizes = size.toString().split('x');
-    const result = [];
-
-    [].forEach.call(splittedSizes, item => {
-      result.push(item * Math.round(this.getDevicePixelRatio()));
-    });
-
-    return result.join('x');
-  }
-
-  generateUrl(operation, size, filters, imgSrc, config): string {
-    const {ultraFast, token, container, queryString} = config;
-    const isUltraFast = ultraFast ? 'https://scaleflex.ultrafast.io/' : 'https://';
-    const cloudUrl = isUltraFast + token + '.' + container + '/';
-
-    return cloudUrl + operation + '/' + size + '/' + filters + '/' + imgSrc + queryString;
-  }
-
-  generateSources(operation, size, filters, imgSrc, isAdaptive, config, isPreview): object[] {
-    const { previewQualityFactor } = config;
-    const sources = [];
-
-    if (isAdaptive) {
-      size.forEach(({ size: nextSize, media: mediaQuery}) => {
-        if (isPreview) {
-          nextSize = nextSize.split('x').map(sizeNext => Math.floor(sizeNext / previewQualityFactor)).join('x');
-        }
-
-        sources.push({ mediaQuery, srcSet: this.generateSrcset(operation, nextSize, filters, imgSrc, config) });
-      });
-    } else {
-      if (isPreview) {
-        size = size.split('x').map(sizeNext => Math.floor(sizeNext / previewQualityFactor)).join('x');
-      }
-
-      sources.push({
-        srcSet: this.generateSrcset(operation, size, filters, imgSrc, config)
-      });
-    }
-    return sources;
-  }
-
-  getAdaptiveSize(size, config): object[] {
-    const arrayOfSizes = size.split(',');
-    const sizes = [];
-
-    arrayOfSizes.forEach(str => {
-      const groups = str.match(/(([a-z_][a-z_]*)|(\([\S\s]*\)))\s*([0-9xp]*)/);
-      const media = groups[3] ? groups[3] : config.presets[groups[2]];
-
-      sizes.push({ media, size: groups[4] });
-    });
-
-    return sizes;
-  }
-
-  generateSrcset(operation, size, filters, imgSrc, config): string {
-    const [imgWidth, imgHeight] = size.toString().split('x');
-
-    return this.generateImgSrc(operation, filters, imgSrc, imgWidth, imgHeight, 1, config);
-  }
-
-  generateImgSrc(operation, filters, imgSrc, imgWidth, imgHeight, factor, config): string {
-    let imgSize = '' + Math.trunc(imgWidth * factor);
-
-    if (imgHeight) {
-      imgSize += 'x' + Math.trunc(imgHeight * factor);
-    }
-
-    return this.generateUrl(operation, this.getSizeAccordingToPixelRatio(imgSize), filters, imgSrc, config)
-      .replace('http://scaleflex.ultrafast.io/', '')
-      .replace('https://scaleflex.ultrafast.io/', '')
-      .replace('//scaleflex.ultrafast.io/', '')
-      .replace('///', '/');
-  }
-
-  getRatioBySize(size, config): number|null {
-    let width;
-    let height;
-
-    if (typeof size === 'object') {
-      const breakPointSource = this.getBreakPoint(size);
-      const breakPointSize = breakPointSource ? breakPointSource.size : size[0].size;
-
-      [width, height] = breakPointSize.toString().split('x');
-    } else {
-      [width, height] = size.toString().split('x');
-    }
-
-    if (width && height) {
-      return width / height;
-    }
-
-    return null;
-  }
-
-  getBreakPoint(size): any {
-    return [...size].reverse().find(item => matchMedia(item.media).matches);
   }
 
   getWindowInnerWidth(): number {
     return this.isSsr ? 1000 : window.innerWidth;
-  }
-
-  getComputedStyle(elem): any {
-    return this.isSsr ? {} : window.getComputedStyle(elem);
-  }
-
-  getDevicePixelRatio(): number {
-    return this.isSsr ? 1 : (window.devicePixelRatio || 1);
   }
 }
